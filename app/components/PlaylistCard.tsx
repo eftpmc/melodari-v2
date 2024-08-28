54bcfccb071c04ec77434aeb7ca2a44249de93a9
SaturnEFTPMC <zackhhi@gmail.com>
2024-08-28 11:34:39 -0400

    playlist card more info

--- app/components/PlaylistCard.tsx
+++ app/components/PlaylistCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Playlist } from '@/types';
+import { FaSpotify, FaYoutube } from 'react-icons/fa';
+import { SiYoutubemusic } from 'react-icons/si';
 
 interface PlaylistCardProps {
   playlist: Playlist;
@@ -7,6 +9,14 @@ interface PlaylistCardProps {
 }
 
 const PlaylistCard: React.FC<PlaylistCardProps> = ({ playlist, onClick }) => {
+  // Determine the icon based on the source of the playlist
+  const icon =
+    playlist.source === 'spotify' ? (
+      <FaSpotify className="text-green-500 w-6 h-6" />
+    ) : (
+      <SiYoutubemusic className="text-red-500 w-6 h-6" />
+    );
+
   return (
     <label
       htmlFor="playlist-modal"
@@ -22,8 +32,9 @@ const PlaylistCard: React.FC<PlaylistCardProps> = ({ playlist, onClick }) => {
       )}
       <div className="ml-4 flex-1">
         <div className="text-base-content font-semibold">{playlist.title}</div>
-        <div className="text-sm text-gray-500">{/* Additional Info (e.g., Author) */}</div>
+        <div className="text-sm text-gray-500">{playlist.accountName}</div> {/* Additional Info */}
       </div>
+      <div className="ml-4">{icon}</div> {/* Icon added here */}
     </label>
   );
 };
